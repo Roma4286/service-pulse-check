001b8a89f846a89823a769f8dc190fdf3df2cb0e
Roma4286 <lionkrou8@gmail.com>
2026-08-26 13:44:01 +0300

    fix(config): fix PG_HOST and PG_NAME

--- app/config.py
+++ app/config.py
@@ -4,9 +4,9 @@ from pydantic.v1 import BaseSettings
 
 load_dotenv()
 
-PG_HOST = os.getenv('POSTGRES_PORT')
+PG_HOST = os.getenv('POSTGRES_HOST')
 PG_PORT = os.getenv('POSTGRES_PORT')
-PG_NAME = os.getenv('COMPOSE_PROJECT_NAME')
+PG_NAME = os.getenv('POSTGRES_DB')
 PG_USER = os.getenv('POSTGRES_USER')
 PG_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 
